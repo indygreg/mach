0de710b876a8ec215e5a870334f3ab7ff86e6528
Gregory Szorc <gregory.szorc@gmail.com>
2013-05-01 18:51:17 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,11 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from setuptools import setup
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
 
 VERSION = '0.1'
 
@@ -10,7 +14,10 @@ setup(
     name='mach',
     description='CLI frontend to mozilla-central.',
     license='MPL 2.0',
+    author='Gregory Szorc',
+    author_email='gregory.szorc@gmail.com',
     packages=['mach'],
-    version=VERSION
+    version=VERSION,
+    install_requires=['mozprocess', 'blessings'],
 )
 
